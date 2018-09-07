ea15b4a4aeafa485aed11acc6a23964989cd0853
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-06 21:52:51 -0400

    Create OutputAssignment.java

---
+++ Introduction To Programming In Java/OutputAssignment.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ * @author amagicalsoup
+ * Ms. Leung's class
+ * ICS3U
+ * Basic Output
+ */
+public class OutputAssignment {
+	public static void main(String[]args) {
+		//Question 1
+			// a) a comment is in green color, but it depends on the complier
+			// b) a reserved work is in red-ish color
+		
+		// Question 2
+			System.out.println(12);
+			System.out.println(13);
+			System.out.println(14);
+			System.out.println(15);
+		
+		//Question 3
+			String first = "James";
+			String last = "Su";
+			System.out.println(first+" "+last);
+			
+		// Question 4
+			String a = "The area code for Toronto is ";
+			String b = "416";
+			System.out.println(a+b);
+			
+		// Question 5
+			int product = 3*4;
+			System.out.println(product);
+			
+		// Question 6
+			int rem = 5%2;
+			System.out.println(rem);
+			
+		// Question 7
+			for(int i = 0; i < 10; i++) {
+				System.out.println("Hello");
+			}
+	}
+}
