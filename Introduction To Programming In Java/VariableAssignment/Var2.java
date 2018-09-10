28501a055ee7be553a3dd0374a94b85827c6ed3d
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-10 18:56:48 -0400

    Create Var2.java

---
+++ Introduction To Programming In Java/VariableAssignment/Var2.java
@@ -0,0 +1,17 @@
+package VariablesAssignment;
+/**
+ *@author amagicalsoup
+ *@author James Su
+ *@Teacher Ms Leung
+ *@ClassCode ICS3U
+ *@Class Variables
+ *@Date September 10th, 2018
+ */
+public class Var2 {
+	public static void main(String[]args) {
+		int num1 = 10; // set num1 as 10
+		int num2 = 20; // set num2 as 20
+		int product = num1 * num2; // set product as num1 * num2
+		System.out.println(product);  // prints the product
+	}
+}
