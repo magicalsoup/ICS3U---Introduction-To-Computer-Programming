0476410954aea08c7ac84d2ddc9491d5c79e8125
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-10 11:51:32 -0400

    Update OutputAssignment.java

--- Introduction To Programming In Java/OutputAssignment.java
+++ Introduction To Programming In Java/OutputAssignment.java
@@ -1,50 +1,50 @@
-	/**
-	 * @author amagicalsoup
-	 * @author James Su
-	 * @Teacher Ms. Leung's class
-	 * @ClassCode ICS3U
-	 * @class Basic Output
-	 * @Date September 7th, 2018
-	 */
-	public class OutputAssignment {
-		 public static void main(String[]args) {
-		  //Out 1 / Question 1
-		   // a) a comment is in green color, but it depends on the IDE, but in Dr.java its green
-		   // b) a reserved word is in blue color, also depends on the IDE, but in Dr.java its blue
-		  
-		  // Out 2 / Question 2
-		   // prints numbers from 12 to 15 included, each on a separate line
-		   System.out.println(12); // prints 12 on a separate line
-		   System.out.println(13); // prints 13 on a separate line
-		   System.out.println(14); // prints 14 on a separate line
-		   System.out.println(15); // prints 15 on a separate line
-		  
-		  //Out 3 / Question 3
-		   // prints my first and last name
-		   String first = "James"; // assign string frist as my first name
-		   String last = "Su"; // assign string last as my last name
-		   System.out.println(first+" "+last); // concatentate the strings and prints it
-		   
-		  // Out 4 / Question 4
-		   // prints a combined string
-		   String a = "The area code for Toronto is "; // assign string a
-		   String b = "416"; // assign string b
-		   System.out.println(a+b); // concatentate the strings together and prints it
-		   
-		  // Out 5 / Question 5
-		   // prints the product of 3*4
-		   int product = 3*4; // assign the value of product to 3*4 which is 12
-		   System.out.println(product); // prints the product
-		   
-		  // Out 6 / Question 6
-		   // prints the remainder of 5/2
-		   int rem = 5%2; // assign rem as the remainder of 5/2
-		   System.out.println(rem); // prints rem
-		   
-		  // Out 7 / Question 7
-		   // prints Hello 10 times each on a separate line
-		   for(int i = 0; i < 10; i++) { // for loop that runs 10 times
-		    System.out.println("Hello"); // prints hello on each time on a separate line
-		  }
-	 }
+ /**
+  * @author amagicalsoup
+  * @author James Su
+  * @Teacher Ms. Leung's class
+  * @ClassCode ICS3U
+  * @class Basic Output
+  * @Date September 7th, 2018
+  */
+ public class OutputAssignment {
+   public static void main(String[]args) {
+    //Out 1 / Question 1
+     // a) a comment is in green color, but it depends on the IDE, but in Dr.java its green
+     // b) a reserved word is in blue color, also depends on the IDE, but in Dr.java its blue
+    
+    // Out 2 / Question 2
+     // prints numbers from 12 to 15 included, each on a separate line
+     System.out.println(12); // prints 12 on a separate line
+     System.out.println(13); // prints 13 on a separate line
+     System.out.println(14); // prints 14 on a separate line
+     System.out.println(15); // prints 15 on a separate line
+    
+    //Out 3 / Question 3
+     // prints my first and last name
+     String first = "James"; // assign string first as my first name
+     String last = "Su"; // assign string last as my last name
+     System.out.println(first+" "+last); // Concatenate the strings and prints it
+     
+    // Out 4 / Question 4
+     // prints a combined string
+     String a = "The area code for Toronto is "; // assign string a
+     String b = "416"; // assign string b
+     System.out.println(a+b); // Concatenate the strings together and prints it
+     
+    // Out 5 / Question 5
+     // prints the product of 3*4
+     int product = 3*4; // assign the value of product to 3*4 which is 12
+     System.out.println(product); // prints the product
+     
+    // Out 6 / Question 6
+     // prints the remainder of 5/2
+     int remainder = 5%2; // assign remainder as the remainder of 5/2
+     System.out.println(remainder); // prints remainder
+     
+    // Out 7 / Question 7
+     // prints Hello 10 times each on a separate line
+     for(int i = 0; i < 10; i++) { // for loop that runs 10 times
+      System.out.println("Hello"); // prints hello on each time on a separate line
+    }
+  }
 }
