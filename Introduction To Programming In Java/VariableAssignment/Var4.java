8f1d2530e3f687dc6aee6110dbadd8dd23e4172b
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-10 19:13:50 -0400

    Create Var4.java

---
+++ Introduction To Programming In Java/VariableAssignment/Var4.java
@@ -0,0 +1,23 @@
+package VariablesAssignment;
+/**
+ *@author amagicalsoup
+ *@author James Su
+ *@Teacher Ms Leung
+ *@ClassCode ICS3U
+ *@Class Variables
+ *@Date September 10th, 2018
+ */
+import java.util.Scanner; // import the Scanner
+public class Var4 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in); // name the Scanner
+		System.out.println("Please Enter Two Numbers "); // ask user to enter two numbers
+		System.out.println("Please Enter The First Number : "); // prompt user for first number
+		int a = sc.nextInt(); // gets first number
+		System.out.println("Please Enter The Second Number : "); // prompt user for second number	
+		int b = sc.nextInt(); // gets second number
+		int result = a + b; // adds a and b
+		System.out.println(result); // prints result
+		sc.close(); // close the Scanner
+	}
+}
