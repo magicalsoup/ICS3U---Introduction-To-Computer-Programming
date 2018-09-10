dc2412e51b1a53f1c858b8899ab7863dd525bd0b
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-10 18:56:20 -0400

    Create Var1.java

---
+++ Introduction To Programming In Java/VariableAssignment/Var1.java
@@ -0,0 +1,15 @@
+package VariablesAssignment;
+/**
+ *@author amagicalsoup
+ *@author James Su
+ *@Teacher Ms Leung
+ *@ClassCode ICS3U
+ *@Class Variables
+ *@Date September 10th, 2018
+ */
+public class Var1 {
+	public static void main(String[] args) {
+		int num = 5; // set num as 5
+		System.out.println(num); // print num
+		}
+}
