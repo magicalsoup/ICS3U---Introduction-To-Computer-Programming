35d243834b9bff0b64bc4737ae8e84b65471dd14
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-10 18:57:40 -0400

    Create Var3.java

---
+++ Introduction To Programming In Java/VariableAssignment/Var3.java
@@ -0,0 +1,21 @@
+package VariablesAssignment;
+/**
+ *@author amagicalsoup
+ *@author James Su
+ *@Teacher Ms Leung
+ *@ClassCode ICS3U
+ *@Class Variables
+ *@Date September 10th, 2018
+ */
+import java.util.Scanner; // import the Scanner
+public class Var3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); // name the Scanner
+		System.out.println("Please Enter Your Name "); // prompt user for name
+		String name = sc.next(); // gets user's name
+		System.out.println("Please Enter Your Age "); // prompt user for age
+		int age = sc.nextInt(); // gets user's age
+		System.out.println("Name : " + name + " Age : " + age); // prints the age
+		sc.close(); // close the Scanner
+	}
+}
