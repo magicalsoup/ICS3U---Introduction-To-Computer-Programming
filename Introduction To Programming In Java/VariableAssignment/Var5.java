bc86232eb2cecffc06fe08a73ae72e83c296d44d
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-11 21:40:15 -0400

    Update Var5.java

--- Introduction To Programming In Java/VariableAssignment/Var5.java
+++ Introduction To Programming In Java/VariableAssignment/Var5.java
@@ -10,7 +10,7 @@ package VariablesAssignment;
 import java.util.Scanner; // import Scanner
 public class Var5 {
 	public static void main(String[]args) {
-		Scanner sc = new Scanner(System.in); // name the Scanner;
+		Scanner sc = new Scanner(System.in); // creating the object Scanner and naming it
 		System.out.println("Please Enter The First Number ");
 		int num = sc.nextInt(); // gets user's number
 		int result = num*2; // double the user's number
