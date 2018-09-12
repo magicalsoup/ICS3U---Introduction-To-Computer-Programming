b2b13a07aee6db9f939d9271011e5fc8e674714a
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-11 21:42:20 -0400

    Update Var9.java

--- Introduction To Programming In Java/VariableAssignment/Var9.java
+++ Introduction To Programming In Java/VariableAssignment/Var9.java
@@ -15,7 +15,8 @@ public class Var9 {
 		String first = sc.next(); // gets user's first name and stores it
 		System.out.println("What Is Your Last Name?"); // ask user for their last name
 		String last = sc.next(); // gets user's last name and stores it
-		System.out.println("Hello "+first+" "+last+"! How are you?");// prints 'Hello' with their full name + 'How are you?'
+		System.out.println("Hello " + first + " " + last + "! How are you?");// prints 'Hello' with their full name 
+																			 // and adds a 'How are you?' at the end
 		sc.close(); // close the Scanner
 	}
 }
