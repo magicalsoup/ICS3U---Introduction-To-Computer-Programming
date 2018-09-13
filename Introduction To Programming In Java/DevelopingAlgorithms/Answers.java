2e651cace2a80667a8fa51ab0e8e181c0ce29c32
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-13 19:52:46 -0400

    Create Answers.java

---
+++ Introduction To Programming In Java/DevelopingAlgorithms/Answers.java
@@ -0,0 +1,53 @@
+package DevelopingAlgorithms;
+
+public class Answers {
+	/**
+		Question 1
+			- prompt user for the number of bars
+			- get the number of bars and store it in a Integer variable
+			- print "The Cost of The Purchase is $ " + 2 * the number of bars
+		Question 2
+			- prompt user for the length
+			- get length and store it in a Integer variable
+			- prompt user for the width
+			- get width and store it in a Integer variable
+			- if length * width is smaller than or equal to 100
+				print "The cost is $ " + length*width*12
+			- else
+				print "The cost is $ " + length*width*10
+		Question 3
+			- while ( there's still more users ) 
+				- prompt the user for the year that they were born in
+				- get the year and store it in a Integer variable
+				- print "You are " + (2018 - year) + " years old"
+				- print "You will be " + (2020 - year) + " years old in 2020"
+		Question 4
+			- prompt user for the radius of the circle
+			- get the radius and store it in a double variable
+			- print "The area of the circle is: " + radius*radius*PI 
+		Question 5
+			- For ( Integer i equals to 2, i is smaller than or equal to 100 , i adds 2 each time )
+					- print i
+		Question 6
+			- declare a double variable called sum and assign a value of 0 to it
+		 	- For (Integers i equals to 0 , i is smaller than or equal to 25 , i adds 1 each time )
+		 			- sum adds the inputed number
+		 	- sum = sum divided by 25
+		 	print "The average on the exam was: " + sum
+		Question 7
+			- prompt user for a number
+			- get the number and store it in a Integer variable
+			- if the number % by 2 equals 0 or is Even
+				- print number + " is Even"
+			- else 
+				- print number + " is Odd"
+		Question 8
+			while ( the end command is inputed )
+				- prompt user for a number
+				- get the number and store it in a Integer variable
+				- if the number % by 2 equals 0 or is Even
+					- print number + " is Even"
+				- else 
+					- print number + " is Odd"
+	 */
+}
