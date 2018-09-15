128cc1b315cc2ab14d3e88657e086e934152fb66
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-14 22:27:28 -0400

    Create Sel3.java

---
+++ Introduction To Programming In Java/SelectionsAssignment/Sel3.java
@@ -0,0 +1,32 @@
+package SelectionsAssignment;
+import java.util.Scanner; // import the Scanner
+/**
+ *@author amagicalsoup
+ *@author James Su
+ *@Teacher Ms Leung
+ *@ClassCode ICS3U
+ *@Class Variables
+ *@Date September 14th, 2018
+ */
+public class Sel3 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in); // creating the object Scanner and naming it
+		System.out.println("Please Enter a Temperature: "); // prompt the user for the temperature
+		int temperature = sc.nextInt(); // get the temperature and store it
+		// I think the codes below are pretty self explanatory ( the logic of the if loops and etc.)
+		if(temperature > 30) {
+			System.out.println("hot");
+		}
+		if(temperature >= 20 && temperature <= 30) {
+			System.out.println("comfortable");
+		}
+		if(temperature >= 10 && temperature <= 19) {
+			System.out.println("cool");
+		}
+		if(temperature <=9) {
+			System.out.println("cold");
+		}
+		//-------------------------------------------------------------------------------------------//
+		sc.close(); // close the Scanner
+	}
+}
