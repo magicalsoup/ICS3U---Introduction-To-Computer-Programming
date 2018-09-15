0295e35ea4580629b832515b418ac806277651dd
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-14 22:49:00 -0400

    Update Sel1.java

--- Introduction To Programming In Java/SelectionsAssignment/Sel1.java
+++ Introduction To Programming In Java/SelectionsAssignment/Sel1.java
@@ -11,7 +11,7 @@ import java.util.Scanner; // import the Scanner
 public class Sel1 {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in); // creating the object Scanner and naming it
-		System.out.println("Please Enter a Number between 1 and 10: "); // prompt the user for a number
+		System.out.println("Please Enter A Number Between 1 And 10: "); // prompt the user for a number
 		int num = sc.nextInt(); // get the number and store it
 		if(num >= 5) { // if number is bigger than or equal to 5
 			System.out.println("You won!");
