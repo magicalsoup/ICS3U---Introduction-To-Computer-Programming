b1495ed1eba671bf2b41ddd697b26763b843ddd9
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-14 22:53:40 -0400

    Create Sel9.java

---
+++ Introduction To Programming In Java/SelectionsAssignment/Sel9.java
@@ -0,0 +1,26 @@
+package SelectionsAssignment;
+import java.util.Scanner; // import the Scanner
+/**
+ *@author amagicalsoup
+ *@author James Su
+ *@Teacher Ms Leung
+ *@ClassCode ICS3U
+ *@Class Selections
+ *@Date September 14th, 2018
+ */
+public class Sel9 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in); // creating the object Scanner and naming it
+		System.out.println("Please Enter Your First Mark That is Between 1 - 100: "); // prompt user for their first mark
+		int firstMark = sc.nextInt(); // get the first mark and store it
+		System.out.println("Please Enter Your Second Mark That is Between 1 - 100: "); // prompt user for their second mark
+		int secondMark = sc.nextInt(); // get the second mark and store it
+		if(firstMark < secondMark) { // if the first mark is lower than the second mark
+			System.out.println(firstMark);
+		}
+		else { // if the second mark is lower than the first mark OR both are equal
+			System.out.println(secondMark);
+		}
+		sc.close(); // close the Scanner
+	}
+}
