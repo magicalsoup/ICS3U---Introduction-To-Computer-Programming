385661180e9a298f66f751f4d656d7b48f8ef9f3
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-14 22:15:03 -0400

    Update Answers.java

--- Introduction To Programming In Java/DevelopingAlgorithms/Answers.java
+++ Introduction To Programming In Java/DevelopingAlgorithms/Answers.java
@@ -1,16 +1,23 @@
 package DevelopingAlgorithms;
 
 public class Answers {
+	/*
+	Key points : Ms leung or Whomever is marking this, PLEASE READ THIS FIRST
+		when assigning a variable, I'm also declaring it as well eg. int a = sc.nextInt(); <- this declares and assigns a value 
+		to the variable at the same time
+		I tried to explain loops best as i can
+		I hope my algorithms are clear and straigh forward enough
+	*/
 	/**
 		Question 1
 			- prompt user for the number of bars
-			- get the number of bars and store it in a Integer variable
-			- print "The Cost of The Purchase is $ " + 2 * the number of bars
+			- get the number of bars and store it in a Integer variable called num_of_bars
+			- print "The Cost of The Purchase is $ " + 2 * num_of_bars
 		Question 2
 			- prompt user for the length
-			- get length and store it in a Integer variable
+			- get length and store it in a Integer variable called length
 			- prompt user for the width
-			- get width and store it in a Integer variable
+			- get width and store it in a Integer variable called width
 			- if length * width is smaller than or equal to 100
 				print "The cost is $ " + length*width*12
 			- else
@@ -18,12 +25,12 @@ public class Answers {
 		Question 3
 			- while ( there's still more users ) 
 				- prompt the user for the year that they were born in
-				- get the year and store it in a Integer variable
+				- get the year and store it in a Integer variable called year
 				- print "You are " + (2018 - year) + " years old"
 				- print "You will be " + (2020 - year) + " years old in 2020"
 		Question 4
 			- prompt user for the radius of the circle
-			- get the radius and store it in a double variable
+			- get the radius and store it in a double variable called radius
 			- print "The area of the circle is: " + radius*radius*PI 
 		Question 5
 			- For ( Integer i equals to 2, i is smaller than or equal to 100 , i adds 2 each time )
@@ -36,7 +43,7 @@ public class Answers {
 		 	print "The average on the exam was: " + sum
 		Question 7
 			- prompt user for a number
-			- get the number and store it in a Integer variable
+			- get the number and store it in a Integer variable called number
 			- if the number % by 2 equals 0 or is Even
 				- print number + " is Even"
 			- else 
@@ -44,7 +51,7 @@ public class Answers {
 		Question 8
 			while ( the end command is inputed )
 				- prompt user for a number
-				- get the number and store it in a Integer variable
+				- get the number and store it in a Integer variable called number 
 				- if the number % by 2 equals 0 or is Even
 					- print number + " is Even"
 				- else 
