203af3050e54225f673651434104dc75f318224d
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-14 22:40:57 -0400

    Create Sel6.java

---
+++ Introduction To Programming In Java/SelectionsAssignment/Sel6.java
@@ -0,0 +1,32 @@
+package SelectionsAssignment;
+import java.util.Scanner; // import the Scanner
+/**
+ *@author amagicalsoup
+ *@author James Su
+ *@Teacher Ms Leung
+ *@ClassCode ICS3U
+ *@Class Selections
+ *@Date September 14th, 2018
+ */
+public class Sel6 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in); // creating the object Scanner and naming it
+		System.out.println("What is your mark?"); // ask user for their mark
+		int mark = sc.nextInt(); // get the user's mark and store it
+		// I think the if statements are pretty self explanatory 
+		if(mark >= 75 && mark <= 100) {
+			System.out.println("Great");
+		}
+		if(mark >= 50 && mark <= 74) {
+			System.out.println("Pass");
+		}
+		if(mark >= 0 && mark <= 49) {
+			System.out.println("Fail");
+		}
+		if(!(mark >= 0 && mark <= 100) ) { // since we can't use 'else if', i did this
+			System.out.println("Invalid");
+		}
+		//-------------------------------------------------------//
+		sc.close(); // close the Scanner
+	}
+}
