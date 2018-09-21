c6f1a95f953358f981c713532cd7a2184bc3a0a9
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-21 12:34:14 -0400

    Update For7.java

--- Introduction To Programming In Java/ForLoopsAssignment/For7.java
+++ Introduction To Programming In Java/ForLoopsAssignment/For7.java
@@ -7,8 +7,10 @@ import java.util.Scanner; // import the Scanner
 public class For7 {
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in); 
+		System.out.println("Please Enter The Starting Number: "); // prompt user for starting number
 		int start = sc.nextInt(); // get the starting number
-		int end = sc.nextInt(); // get the ending number
+		System.out.println("Please Enter The End Number: "); // prompt user for the end number
+		int end = sc.nextInt(); // get the end number
 		int cnt = 0;
 		for(int i = start; i <= end; i++){ // this loop runs from start to end inclusive
 			cnt = i;
