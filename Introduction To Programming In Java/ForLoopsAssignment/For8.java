7638b516ea35e94b2115219c905ddf5f0a3ea6f9
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-21 12:27:21 -0400

    Create For8.java

---
+++ Introduction To Programming In Java/ForLoopsAssignment/For8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; // import the Scanner
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 21th, 2018
+ */
+public class For8 {
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		int sum = 0; // variable to store the sum of the numbers
+		for(int i = 0; i < 5; i++){ // this loops runs 5 times, from 0 to 5
+			int num = sc.nextInt(); // get the input number
+			sum += num; // add up the numbers
+		}
+		System.out.println(sum); // print the sum of the 5 numbers
+		sc.close();
+	}
+}
