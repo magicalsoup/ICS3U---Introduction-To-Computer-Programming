3e5ab2142fa915d869e133dc7199c37e567491a4
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-21 12:05:01 -0400

    Create For4.java

---
+++ Introduction To Programming In Java/ForLoopsAssignment/For4.java
@@ -0,0 +1,12 @@
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 21th, 2018
+ */
+public class For4 {
+	public static void main(String[]args){
+		for(int i = 1; i <=10; i++) { // this loops counts from 1 to 10 inclusive
+			System.out.println(i + ". James"); // prints i and my name
+		}
+	}
+}
