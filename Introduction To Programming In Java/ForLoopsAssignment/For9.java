6af019aad2cf52890a783ef3109edb32ed40a788
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-21 12:30:51 -0400

    Create For9.java

---
+++ Introduction To Programming In Java/ForLoopsAssignment/For9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // import the Scanner
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 21th, 2018
+ */
+public class For9 {
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		double sum = 0; // variable to store the sum of the numbers
+		for(int i = 0; i < 3; i++){ // this loop runs 3 times, from 0 to 3
+			int num = sc.nextInt(); 
+			sum += num;// add up the numbers
+		}
+		double average =  (sum/3.0); // get the average of the 3 numbers
+		System.out.println("The Average Of The Numbers is " + average); // print the average
+		sc.close();
+	}
+}
