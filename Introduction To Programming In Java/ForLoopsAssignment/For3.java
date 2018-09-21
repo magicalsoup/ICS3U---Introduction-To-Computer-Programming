fd87d80e21540412b357556c2b8fc07cdc454bbe
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-21 12:03:18 -0400

    Create For3.java

---
+++ Introduction To Programming In Java/ForLoopsAssignment/For3.java
@@ -0,0 +1,14 @@
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 21th, 2018
+ */
+public class For3 {
+	public static void main(String[]args){
+		int counter = 0;
+		for(counter = 0; counter <= 100; counter++){ // this loop counts from 0 to 100
+		}
+		for(counter = 100; counter>=0; counter--){ // this loop counts from 100 back down to 0	
+		}
+	}
+}
