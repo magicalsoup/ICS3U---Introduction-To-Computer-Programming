17d62186970b0323abd6786b515a040fb136ad9d
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-21 12:32:08 -0400

    Update For5.java

--- Introduction To Programming In Java/ForLoopsAssignment/For5.java
+++ Introduction To Programming In Java/ForLoopsAssignment/For5.java
@@ -7,7 +7,9 @@ import java.util.Scanner; // import the Scanner
 public class For5 {
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in); 
+		System.out.println("Please Enter A Word: "); // prompt user for a word
 		String word = sc.next(); // ask for the word
+		System.out.println("Please Enter The Number Of Times: "); // prompt user for the number of times
 		int times = sc.nextInt(); // ask for the # of times
 		for(int i = 0; i < times; i++){ // this loop runs 'times' number of times Example: if times = 5, it runs 5 times
 			System.out.println(word); // print the word
