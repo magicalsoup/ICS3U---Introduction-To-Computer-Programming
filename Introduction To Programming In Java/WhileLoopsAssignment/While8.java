7a19498c3077780cda8691f26f87b4064239b933
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-25 11:54:01 -0400

    Create While8.java

---
+++ Introduction To Programming In Java/WhileLoopsAssignment/While8.java
@@ -0,0 +1,19 @@
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 25th, 2018
+ */
+public class While8 {
+	public static void main(String[]args){
+		int num = 1;
+		int sum = 0;
+		while(num <= 10){ // this loop will run as long as num is smaller or equal to 10
+			System.out.println(num);
+			sum += num; // add num to sum
+			num++; // increment num by 1
+		}
+		System.out.println("The Sum Is: " + sum);
+		double average = sum / 10; // calculate the average
+		System.out.println("The Average Is: " + average);
+	}
+}
