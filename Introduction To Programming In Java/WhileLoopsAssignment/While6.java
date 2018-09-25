6fcb60b47f1b8093af4b561a38b9727896e42090
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-25 11:50:19 -0400

    Create While6.java

---
+++ Introduction To Programming In Java/WhileLoopsAssignment/While6.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; // import the Scanner
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 25th, 2018
+ */
+public class While6 {
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please Enter A Word: "); // prompt user for a word
+		String word = sc.next(); // get the user inputed word
+		System.out.println("How Many Times: ?"); // ask user to repeat for how many times
+		int times = sc.nextInt(); // get the user inputed time
+		int index = 1;
+		while(index <= times){ // this loop will run for 'times' amount of times, as long as the index is smaller than or equal to 'times'
+			System.out.println(index + ". " + word); // print the index with the word
+			index++; // increment the index by 1
+		}
+		sc.close();
+	}
+}
