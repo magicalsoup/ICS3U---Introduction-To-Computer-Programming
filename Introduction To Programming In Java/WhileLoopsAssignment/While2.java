dbee84da68b733737715186df5cc8b95053a0da3
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-25 12:02:42 -0400

    Update While2.java

--- Introduction To Programming In Java/WhileLoopsAssignment/While2.java
+++ Introduction To Programming In Java/WhileLoopsAssignment/While2.java
@@ -1,4 +1,3 @@
-import java.util.Scanner; // import the Scanner
 /**
  *@author James Su
  *@Teacher Ms Leung
@@ -6,12 +5,10 @@ import java.util.Scanner; // import the Scanner
  */
 public class While2 {
 	public static void main(String[]args){
-		Scanner sc = new Scanner(System.in);
 		int counter = 0;
-		while(counter < 100){ // this loop will run until counter equals 100, so counter will count from 0 to 99
+		while(counter < 100){ // this loop will run as long counter is smaller than 100
 			System.out.println(counter);
 			counter++; // increment the counter by 1
 		}
-		sc.close();
 	}
 }
