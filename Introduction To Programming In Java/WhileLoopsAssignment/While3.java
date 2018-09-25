75d79913f4a7b7bbb3a1a7d01c3fe8ce2fcb1d02
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-25 12:03:54 -0400

    Update While3.java

--- Introduction To Programming In Java/WhileLoopsAssignment/While3.java
+++ Introduction To Programming In Java/WhileLoopsAssignment/While3.java
@@ -1,4 +1,3 @@
-import java.util.Scanner; // import the Scanner
 /**
  *@author James Su
  *@Teacher Ms Leung
@@ -6,16 +5,14 @@ import java.util.Scanner; // import the Scanner
  */
 public class While3 {
 	public static void main(String[]args){
-		Scanner sc = new Scanner(System.in);
 		int counter = 0;
-		while(counter < 100){ // runs from 0 to 99, the condition is that when counter equals 100, it stops
+		while(counter < 100){ // runs as long counter is smaller than 100
 			System.out.println(counter);
 			counter++; // increment the counter by 1
 		}
-		while(counter >= 0){ // runs from 100 to 0, since previously the counter was 100 from the first while loop, all this does is count back down to 0
+		while(counter >= 0){ // runs as long counter is bigger or equal to 0
 			System.out.println(counter);
 			counter--; // decrease the counter by 1
 		}
-		sc.close();
 	}
 }
