e9d709a5bef305c57a04aee68f00ca3d3ed48623
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-25 19:21:21 -0400

    Update For11.java

--- Introduction To Programming In Java/ForLoopsAssignment/For11.java
+++ Introduction To Programming In Java/ForLoopsAssignment/For11.java
@@ -1,3 +1,4 @@
+package ForLoopsAssignment;
 import java.util.Scanner; // import the Scanner
 /**
  *@author James Su
@@ -7,7 +8,7 @@ import java.util.Scanner; // import the Scanner
 public class For11 {
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in);
-		int low = 9999; // the variable will store the lowest mark
+		int low = 101; // the variable will store the lowest mark
 		for(int i = 0; i < 5; i++){ // this loop runs 5 times, from 0 to 5
 			System.out.println("Please Enter A Mark: "); // prompt user for a mark
 			int mark = sc.nextInt(); // get the inputed mark
