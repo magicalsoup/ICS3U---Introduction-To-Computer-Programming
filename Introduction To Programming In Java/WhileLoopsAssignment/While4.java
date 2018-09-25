192e46a27ee0adc91baee09ef95eaaa71bb3f4c9
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-25 12:04:26 -0400

    Update While4.java

--- Introduction To Programming In Java/WhileLoopsAssignment/While4.java
+++ Introduction To Programming In Java/WhileLoopsAssignment/While4.java
@@ -1,4 +1,3 @@
-import java.util.Scanner; // import the Scanner
 /**
  *@author James Su
  *@Teacher Ms Leung
@@ -6,12 +5,10 @@ import java.util.Scanner; // import the Scanner
  */
 public class While4 {
 	public static void main(String[]args){
-		Scanner sc = new Scanner(System.in);
 		int index = 1;
-		while(index <= 10){ // this loop runs along as index is smaller or equal to 10, this loops runs from 1 to 10 inclusive
+		while(index <= 10){ // this loop runs as long as index is smaller or equal to 10
 			System.out.println(index + ". James Su");
 			index++; // increment the index by 1
 		}
-		sc.close();
 	}
 }
