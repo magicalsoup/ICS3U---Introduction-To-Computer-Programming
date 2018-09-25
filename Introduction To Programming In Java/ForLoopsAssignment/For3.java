0d0298eb105511eb5989942627389b1273535f61
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-25 12:07:05 -0400

    Update For3.java

--- Introduction To Programming In Java/ForLoopsAssignment/For3.java
+++ Introduction To Programming In Java/ForLoopsAssignment/For3.java
@@ -6,9 +6,11 @@
 public class For3 {
 	public static void main(String[]args){
 		int counter = 0;
-		for(counter = 0; counter <= 100; counter++){ // this loop counts from 0 to 100
+		for(counter = 0; counter <100; counter++){ // this loop counts from 0 to 99
+			System.out.println(counter);
 		}
 		for(counter = 100; counter>=0; counter--){ // this loop counts from 100 back down to 0	
+			System.out.println(counter);
 		}
 	}
 }
