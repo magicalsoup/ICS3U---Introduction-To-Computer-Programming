865f684c2e2357a3878b035eb0d205cd859f48b9
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-27 11:41:22 -0400

    Create Do1.java

---
+++ Introduction To Programming In Java/DoLoopAssignment/Do1.java
@@ -0,0 +1,15 @@
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 25th, 2018
+ */
+public class Do1 {
+	public static void main(String[]args){
+		int index = 0;
+		do { // run this part first as long index is smaller than 10
+			System.out.println("James"); 
+			index++; // increment the index by 1
+		}
+		while(index <10); // check condition for do loop
+	}
+}
