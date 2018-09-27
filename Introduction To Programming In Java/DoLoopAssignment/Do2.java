9a02f0f07da74128fa02b4d55b0552567fcdea3d
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-27 11:44:56 -0400

    Create Do2.java

---
+++ Introduction To Programming In Java/DoLoopAssignment/Do2.java
@@ -0,0 +1,15 @@
+/**
+ *@author James Su
+ *@Teacher Ms Leung
+ *@Date September 27th, 2018
+ */
+public class Do2 {
+	public static void main(String[]args){
+		int num = 0; // declare and set num as 0
+		do { // run this first as long as num is smaller than 100
+			System.out.println(num);
+			num++; // increment num by 1
+		}
+		while(num < 100);
+	}
+}
