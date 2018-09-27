a9800078d4ca5dcd9c305370d16b7649f08f218a
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-27 11:44:28 -0400

    Update Do1.java

--- Introduction To Programming In Java/DoLoopAssignment/Do1.java
+++ Introduction To Programming In Java/DoLoopAssignment/Do1.java
@@ -1,11 +1,11 @@
 /**
  *@author James Su
  *@Teacher Ms Leung
- *@Date September 25th, 2018
+ *@Date September 27th, 2018
  */
 public class Do1 {
 	public static void main(String[]args){
-		int index = 0;
+		int index = 0; // declare and set this as 0
 		do { // run this part first as long index is smaller than 10
 			System.out.println("James"); 
 			index++; // increment the index by 1
