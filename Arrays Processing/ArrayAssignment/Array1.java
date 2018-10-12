d34720732348d68f15aeaf9dbdb484d7c2778c92
James <39444003+magicalsoup@users.noreply.github.com>
2018-10-12 11:54:58 -0400

    Update Array1.java

--- Arrays Processing/ArrayAssignment/Array1.java
+++ Arrays Processing/ArrayAssignment/Array1.java
@@ -5,10 +5,12 @@
  */
 public class Array1 {
 	public static void main(String[]args){ 
-		int arr[] = new int[20]; // create a new array of integers
-		for(int i = 0; i < arr.length; i++) // for loop that runs 20 times
+		int arr[] = new int[20]; // create the array with size of 20
+		for(int i = 0; i < arr.length; i++) { // for loop that runs 20 times
 			arr[i] = i + 1; // array at index i equals i + 1
-		for(int i = 0; i < arr.length; i++) // for loop that runs 20 times
+		}
+		for(int i = 0; i < arr.length; i++) { // for loop that runs 20 times
 			System.out.print(arr[i] + " "); // print the integers in the array
+		}
 	}
 }
