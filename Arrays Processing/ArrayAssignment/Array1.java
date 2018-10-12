f892b6d25423c3694ef45ae0b9cff496c6a905ad
James <39444003+magicalsoup@users.noreply.github.com>
2018-10-12 11:40:17 -0400

    Update Array1.java

--- Arrays Processing/ArrayAssignment/Array1.java
+++ Arrays Processing/ArrayAssignment/Array1.java
@@ -1,4 +1,3 @@
-import java.util.Scanner; // import the Scanner
 /**
  * @author James Su
  * @Date Oct 12, 2018
@@ -6,12 +5,10 @@ import java.util.Scanner; // import the Scanner
  */
 public class Array1 {
 	public static void main(String[]args){ 
-		Scanner sc = new Scanner(System.in); // create the Scanner
 		int arr[] = new int[20]; // create a new array of integers
 		for(int i = 0; i < arr.length; i++) // for loop that runs 20 times
 			arr[i] = i + 1; // array at index i equals i + 1
 		for(int i = 0; i < arr.length; i++) // for loop that runs 20 times
 			System.out.print(arr[i] + " "); // print the integers in the array
-		sc.close(); // close the Scanner
 	}
 }
