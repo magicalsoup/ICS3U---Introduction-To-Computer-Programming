a878d4ad53f68bfe889896dcb85633f0195e9f4a
James <39444003+magicalsoup@users.noreply.github.com>
2018-10-12 23:13:42 -0400

    Create Array5.java

---
+++ Arrays Processing/ArrayAssignment/Array5.java
@@ -0,0 +1,39 @@
+import java.util.Scanner; // import the Scanner
+/**
+ * @author James Su
+ * @Date Oct 12, 2018
+ * @Teacher Ms Leung
+ */
+public class Array5 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in); // create the Scanner
+		String firstNames[] = new String[10]; // create a string array with size of 10
+		String lastNames[] = new String[10]; // create a string array with size of 10
+		int marks[] = new int[10]; // create a integer array with size of 10
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Please Enter Your First Name: "); // prompt user
+			firstNames[i] = sc.next(); // firstNames at index i is equal to the user input
+			System.out.println("Please Enter Your Last Name: "); // prompt user
+			lastNames[i] = sc.next(); // lastNames at index i is equal to the user input
+			System.out.println("Please Enter Your Mark: "); // prompt user
+			marks[i] = sc.nextInt(); // marks at index i is equal to user input
+		}
+		System.out.println("Print The Ones That Passed Or Failed? Pass / Fail "); // prompt user
+		String respond = sc.next(); // get user input
+		if(respond.equals("Pass")) { // if respond equals Pass 
+			for(int i = 0; i < 10; i++) { // a for loop that runs 10 times
+				if(marks[i] >= 50) { // if marks at index i is bigger or equal to 50
+					System.out.println(firstNames[i] + " " + lastNames[i]); // print firstNames at index i and lastNames at index i
+				}
+			}
+		}
+		else { // else
+			for(int i = 0; i < 10; i++) { // a for loop that runs 10 tims
+				if(marks[i] < 50) { // if marks at index i is smaller than 50
+					System.out.println(firstNames[i] + " " + lastNames[i]); // peinr firstNames at index i and lastNames at index i
+				}
+			}
+		}
+		sc.close(); // close the Scanner
+	}
+}
