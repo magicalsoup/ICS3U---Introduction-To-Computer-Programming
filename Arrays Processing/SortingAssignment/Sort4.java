194c9dfe4cc233ec0f3ff4c0d56bcd4bded1e951
James <39444003+magicalsoup@users.noreply.github.com>
2018-10-25 12:16:05 -0400

    Create Sort4.java

---
+++ Arrays Processing/SortingAssignment/Sort4.java
@@ -0,0 +1,14 @@
+/**
+ * @author James Su
+ * @Teacher Ms Leung
+ * @Date Oct 25th 2018
+ */
+public class Sort4 {
+	/**
+	 * # Of Items In The Array | # of Comparisons
+	 * 			5 						10
+	 * 			10     					45
+	 * 			20						190
+	 * 			n						n(n-1)/2	 
+	 */
+}
