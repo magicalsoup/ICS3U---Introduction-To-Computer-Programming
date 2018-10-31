4965306cd106292eff9a8ab9545ce45ac135d9c2
James <39444003+magicalsoup@users.noreply.github.com>
2018-10-31 12:35:22 -0400

    Create DataTypes1.java

---
+++ Arrays Processing/StringExerciseAssignment/DataTypes1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner; // import the Scanner
+/**
+ * @author James Su
+ * @Date Oct 31st 2018
+ * @Teacher Ms Leung
+ */
+public class DataTypes1 {
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in); // create the Scanner
+		System.out.println("Please Enter True or False: "); // prompt user
+		String s = sc.next(); // get user input
+		boolean flag = false; // set flag to false
+		if(s.equals("true")){ // if s equals true
+			flag = true; // assing the value
+		}
+		System.out.println(flag); // print flag
+		sc.close(); // close the Scanner
+	}
+}
