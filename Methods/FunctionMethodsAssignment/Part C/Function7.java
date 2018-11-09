6bef26ab4a96cdbcb4bd1fe85417899330593857
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-09 12:17:37 -0500

    Update Function7.java

--- Methods/FunctionMethodsAssignment/Part C/Function7.java
+++ Methods/FunctionMethodsAssignment/Part C/Function7.java
@@ -11,15 +11,23 @@ public class Function7 {
 		System.out.println("Enter How Many Cases: "); // prompt user
 		int n = sc.nextInt(); // get user input
 		for(int i = 0; i < n; i++) { // runs for n times
-			System.out.println("Please Enter The X - Coordinate For the First Point"); // prompt user
+			System.out.println("Please Enter The Start X - Coordinate For the First Point"); // prompt user
 			int x1 = sc.nextInt(); // get user input
-			System.out.println("Please Enter The Y - Coordinate For the First Point"); // prompt user
+			System.out.println("Please Enter The Start Y - Coordinate For the First Point"); // prompt user
 			int y1 = sc.nextInt(); // get user input
-			System.out.println("Please Enter The X - Coordinate For the Second Point"); // prompt user
+			System.out.println("Please Enter The End X - Coordinate For the First Point"); // prompt user
 			int x2 = sc.nextInt(); // get user input
-			System.out.println("Please Enter The Y - Coordinate For the Second Point"); // prompt user
+			System.out.println("Please Enter The End Y - Coordinate For the First Point"); // prompt user
 			int y2 = sc.nextInt(); //get user input
-			if(parallel(x1, y1, x2, y2)) { // call the method, and if its true
+			System.out.println("Please Enter The Start X - Coordinate For the Second Point"); // prompt user
+			int x12 = sc.nextInt(); // get user input
+			System.out.println("Please Enter The Start Y - Coordinate For the Second Point"); // prompt user
+			int y12 = sc.nextInt(); // get user input
+			System.out.println("Please Enter The End X - Coordinate For the Second Point"); // prompt user
+			int x22 = sc.nextInt(); // get user input
+			System.out.println("Please Enter The End Y - Coordinate For the Second Point"); // prompt user
+			int y22 = sc.nextInt(); //get user input
+			if(slope(x1, y1, x2, y2) == slope(x12, y12, x22, y22)) { // call the method, and if its true
 				System.out.println("Is Parallel"); // print
 			}
 			else {
@@ -28,9 +36,9 @@ public class Function7 {
 		}
 		sc.close(); // close the Scanner
 	}
-	static boolean parallel(int x1, int y1, int x2, int y2) { // method to check if its a parallel line
-		if(x1 == 0 || x2 == 0) return false; // no dividing by zeroes
-		return y1 / x1 == y2 / x2; // return the boolean of this expression
+	static double slope(int x1, int y1, int x2, int y2) { // method to calculate distance of 2 points
+		if(x2 - x1 == 0) return 0; // in case, so no errors return 0
+		return (y2 - y1) / (x2 - x1); // return the calculation / distance
 	}
 	/**
 	 * @Algorithm
@@ -40,7 +48,7 @@ public class Function7 {
 	 * get the end points of the 2 points
 	 * call the method and check if they are parallel
 	 * print according to the method/boolean
-	 * the method: check if the first slope equals the second slope, assuming the start points are 0 as 
-	 * the user does not enter the start points
+	 * the method: check if the first slope equals the second slope
+	 * the method: calculate the slope
 	 */
 }
