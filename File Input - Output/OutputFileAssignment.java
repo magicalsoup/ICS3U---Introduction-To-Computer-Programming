aed4b7aa363a6a6c35e0344ab0038ff9ccef1835
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-27 13:41:27 -0500

    Create OutputFileAssignment.java

---
+++ File Input - Output/OutputFileAssignment.java
@@ -0,0 +1,15 @@
+import java.io.File; // import file
+import java.io.PrintWriter; // import printWriter
+/**
+ * @author James Su
+ * @Teacher Ms Leung
+ * @Date Nov 27, 2018
+ */
+public class OutFile1 { // main class
+	public static void main(String[]args) throws Exception { //main method
+		File myFile = new File("file1.txt"); // create file
+		PrintWriter out = new PrintWriter(myFile); // create printWriter
+		out.println("James Su"); // output my name into the file
+		out.close(); // close the print writer
+	}
+}
