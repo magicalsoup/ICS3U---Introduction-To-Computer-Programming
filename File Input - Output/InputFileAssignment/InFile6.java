ee8d4c4fd2b625434741b4ca13dc99e6a7a718e9
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-30 11:58:28 -0500

    Update InFile6.java

--- File Input - Output/InputFileAssignment/InFile6.java
+++ File Input - Output/InputFileAssignment/InFile6.java
@@ -9,13 +9,13 @@ public class InFile6 { // main class
 	public static void main(String[]args) throws Exception { //main method
 		File myFile = new File("file7.txt"); // create file
 		Scanner sc = new Scanner(myFile); // create Scanner
-		String arr[] = new String[15]; // create an array of size 10 
+		String arr[] = new String[15]; // create an array of size 15
 		int index = 0; // set index to 0
 		while(sc.hasNext()){ // while there's more input
 			arr[index] = sc.next(); // set the input into the array
 			index++; // increment index by 1
 		} 
-		for(int i = arr.length - 1; i >= 0; i--){ // runs 10 times
+		for(int i = arr.length - 1; i >= 0; i--){ // runs 15 times
 			System.out.print(arr[i] + " "); // print arr[i]
 		}
 		sc.close(); // close the Scanner
